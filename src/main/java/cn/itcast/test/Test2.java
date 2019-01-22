95f0bd8916067d1590928f92cf3454049c3ac0dd
Dadong <285469872@qq.com>
2019-01-22 10:54:44 +0800

    zhe shi wo de ce shi 2

--- src/main/java/cn/itcast/test/Test2.java
+++ src/main/java/cn/itcast/test/Test2.java
@@ -2,8 +2,8 @@ package cn.itcast.test;
 
 public class Test2 {
 	public void ceshi2() {
-		System.out.println("maybe i know this");
-		System.out.println("ok£¬i got it");
-		System.out.println("I just want to test");
+		System.out.println("zheshiceshi3");
+		System.out.println("zheshiceshi3");
+		System.out.println("zheshiceshi3");
 	}
 }
