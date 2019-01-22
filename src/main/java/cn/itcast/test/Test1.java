05a1fae1263a6175ee6f122af92cf924bf1c68c3
Dadong <285469872@qq.com>
2019-01-22 10:18:21 +0800

    this is the first commit of Demo1

---
+++ src/main/java/cn/itcast/test/Test1.java
@@ -0,0 +1,9 @@
+package cn.itcast.test;
+
+public class Test1 {
+
+	public void TestOne() {
+		System.out.println("hello+1");
+	}
+}
+
---
+++ src/test/java/cn/itcast/Defalut/ceshi1.java
@@ -0,0 +1,8 @@
+package cn.itcast.Defalut;
+
+public class ceshi1 {
+	
+	public void ceshijieguo() {
+		System.out.println("this is test");
+	}
+}
