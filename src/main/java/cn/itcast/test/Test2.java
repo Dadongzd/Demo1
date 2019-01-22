e0d96aa53d816718b6b00248f7ab7eddc0d30f48
Dadong <285469872@qq.com>
2019-01-22 10:22:18 +0800

    hello,this is my second commit of my Demo1

---
+++ src/main/java/cn/itcast/test/Test2.java
@@ -0,0 +1,7 @@
+package cn.itcast.test;
+
+public class Test2 {
+	public void ceshi2() {
+		System.out.println("why not any change");
+	}
+}
--- src/test/java/cn/itcast/Defalut/ceshi1.java
+++ src/test/java/cn/itcast/Defalut/ceshi1.java
@@ -3,6 +3,6 @@ package cn.itcast.Defalut;
 public class ceshi1 {
 	
 	public void ceshijieguo() {
-		System.out.println("this is test");
+		System.out.println("this is my test");
 	}
 }
